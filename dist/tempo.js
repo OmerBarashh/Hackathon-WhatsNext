a8b8a87a541b9985db77309d782d9a805bd652e8
guynuriel <51605830+guynuriel@users.noreply.github.com>
2019-07-17 09:30:54 +0300

    Revert "finish day 1"

--- dist/tempo.js
+++ dist/tempo.js
@@ -1,100 +1,44 @@
-class TempManager {
+
+class TempoManager {
     constructor() {
-        this.standby = []
-        this.watchedShows = []
-        this.blackList = []
-        this.wishList = []
+        this.watchedShowsData = [],
+            this.blackListData = [],
+            this.wishListData = []
 
     }
 
-    async getShowData(showName) {
-        const res = await $.get(`/show/${showName}`)        
-        this.standby.push(res)
-        console.log(res)
-       
-    }
-
-
-
-    async watchedShowsDB() {
-        let data = await $.get('/watchedShows',function(res){
-         return  res
+    watchedShows(showName) {
+        $.get(`/show/${showName}`, function (response) {
+            this.watchedShowsData.push(response)
         })
-    data.forEach(d => this.watchedShows.push(d));
     }
 
-    async blackListDB() {
-        let data = await $.get('/blackList',function(res){
-         return  res
+    blackList(showName) {
+        $.get(`/show/${showName}`, function (response) {
+            this.blackListData.push(response)
         })
-    data.forEach(d => this.blackList.push(d));
     }
 
-
-    async wishListDB() {
-        let data = await $.get('/wishList',function(res){
-         return  res
-        })
-    data.forEach(d => this.wishList.push(d));
-    }
-
-
-    wishListSave(showName) {
-        const showD =this.standby.find(s => s.name === showName)
-         $.post('/wishList', showD ,function(data,status){
-            console.log("status:",status)
-            console.log('data:',data)
+    wishList(showName) {
+        $.get(`/show/${showName}`, function (response) {
+            this.wishListData.push(response)
         })
     }
 
 
-    
-    blackListSave(showName) {
-        const showD =this.standby.find(s => s.name === showName)
-         $.post('/blackList', showD ,function(data,status){
-            console.log("status:",status)
-            console.log('data:',data)
-        })
-    }
-    
-    watchedShowSave(showName) {
-        const showD =this.standby.find(s => s.name === showName)
-         $.post('/watchedShow', showD ,function(data,status){
-            console.log("status:",status)
-            console.log('data:',data)
-        })
-    }
+    // $.get(`/shows`, function (response) { })
 
 
+    // $.post(`/show`, Data, function (response) {
+    // })
 
-    wishListRemove(showName) {
+    deleteFromWatched() {
         $.ajax({
-            method:"DELETE",
-            url:`/wishList/${showName}` ,
-            success: function(data){
-                console.log(data)
-            },
-        })
-    }
+            method: "DELETE",
+            url: `/show/${showname}`,
+            success: function () {
 
-    blackListRemove(showName) {
-        $.ajax({
-            method:"DELETE",
-            url:`/city/${showName}` ,
-            success: function(data){
-                console.log(data)
-            },
-        })
-    }
-
-
-    watchedShowsRemove(showName) {
-        $.ajax({
-            method:"DELETE",
-            url:`/city/${showName}` ,
-            success: function(data){
-                console.log(data)
-            },
+            }
         })
     }
 
